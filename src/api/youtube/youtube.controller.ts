c28fdec94adf4f547afff6405d9c8f04eb5547cb
sanghyeonkim <gmhty4345@gmail.com>
2024-06-27 08:54:22 +0900

    🚩 : youtube service

--- src/api/youtube/youtube.controller.ts
+++ src/api/youtube/youtube.controller.ts
@@ -1,10 +1,4 @@
-import {
-  Controller,
-  Get,
-  Param,
-  // UnauthorizedException,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
 import { JwtAccessAuthGuard } from 'src/common/utils/guard/jwtAccessAuthGuard';
 import {
   ApiOperation,
@@ -30,7 +24,16 @@ export class YoutubeController {
     return await this.youtubeService.getPlayLists(payload.id);
   }
 
-  @Get('playlists/items/:playlistId')
+  @Get('/playlist/:playlistId')
+  @ApiOperation({ summary: 'get playlist data' })
+  async playlist(
+    @Payload() playload: PayloadDTO,
+    @Param('playlistId') playlistId: string,
+  ) {
+    return await this.youtubeService.getPlaylist(playload.id, playlistId);
+  }
+
+  @Get('playlist/item/:playlistId')
   @ApiOperation({ summary: 'get playlist items' })
   async itmes(
     @Payload() payload: PayloadDTO,
@@ -39,15 +42,52 @@ export class YoutubeController {
     // PLBJFwQTTuErTJJoLzfsGviKY8LQFyYGOH
     return await this.youtubeService.getPlaylistItems(payload.id, playlistId);
   }
-
+  @Get('playlists/items')
+  async AllItmes(@Payload() paylaod: PayloadDTO) {
+    return await this.youtubeService.getAllPlaylistItems(paylaod.id);
+  }
   @Get('videos')
   @ApiOperation({ summary: 'get search video data' })
-  async search(@Payload() payload: PayloadDTO) {
+  async allVideos(@Payload() payload: PayloadDTO) {
     return await this.youtubeService.getMostVideo(payload.id);
   }
   @Get('likes')
   @ApiOperation({ summary: 'get like video data' })
   async likeVideos(@Payload() payload: PayloadDTO) {
     return await this.youtubeService.getLikeVideo(payload.id);
+  }
+  @Get('dislikes')
+  @ApiOperation({ summary: 'get dislike videos data' })
+  async dislikesVideos(@Payload() payload: PayloadDTO) {
+    return await this.youtubeService.getDislikeVideos(payload.id);
+  }
+  @Get('subscriber')
+  @ApiOperation({ summary: 'get subscriber' })
+  async subscriber(@Payload() payload: PayloadDTO) {
+    return await this.youtubeService.getSubscriber(payload.id);
+  }
+
+  @Get('search')
+  @ApiOperation({ summary: 'get search data' })
+  async search(
+    @Payload() payload: PayloadDTO,
+    @Query() query: { keyword: string },
+  ) {
+    return await this.youtubeService.getSearchData(payload.id, query.keyword);
+  }
+  @Get('recent-video')
+  @ApiOperation({ summary: 'get recent' })
+  async recentVideo(@Payload() payload: PayloadDTO) {
+    return await this.youtubeService.getRecentSearch(payload.id);
+  }
+  @Get('all-rating')
+  @ApiOperation({ summary: 'get dislike and like videos' })
+  async allRating(@Payload() payload: PayloadDTO) {
+    const likeVideos = await this.youtubeService.getLikeVideo(payload.id);
+    const dislikeVideos = await this.youtubeService.getDislikeVideos(
+      payload.id,
+    );
+
+    return { likeVideos, dislikeVideos };
   }
 }
--- src/api/youtube/youtube.service.ts
+++ src/api/youtube/youtube.service.ts
@@ -11,12 +11,22 @@ export class YoutubeService {
     private authService: AuthService,
   ) {}
 
-  async getPlayLists(userId: string): Promise<any> {
+  async getPlayLists(userId: string) {
     const youtube = await this.authService.getYoutubeAPI(userId);
     const response = await youtube.playlists.list({
       part: ['snippet', 'contentDetails', 'status', 'id'],
       mine: true,
-      maxResults: 10,
+      maxResults: 50,
+    });
+    return response.data;
+  }
+
+  async getPlaylist(userId: string, playlistId: string) {
+    const youtube = await this.authService.getYoutubeAPI(userId);
+    const response = await youtube.playlists.list({
+      part: ['snippet', 'contentDetails', 'status', 'id'],
+      id: [playlistId],
+      maxResults: 50,
     });
     return response.data;
   }
@@ -24,12 +34,34 @@ export class YoutubeService {
   async getPlaylistItems(userId: string, playlistId: string) {
     const youtube = await this.authService.getYoutubeAPI(userId);
     const response = await youtube.playlistItems.list({
-      part: ['snippet'],
+      part: ['snippet', 'id', 'contentDetails'],
+      maxResults: 50,
       playlistId,
     });
     return response.data;
   }
+  async getAllPlaylistItems(userId: string) {
+    const youtube = await this.authService.getYoutubeAPI(userId);
 
+    const response = await youtube.playlists.list({
+      part: ['snippet', 'contentDetails', 'status', 'id'],
+      mine: true,
+      maxResults: 50,
+    });
+
+    const result = Promise.all(
+      response.data.items.map(async (o) => {
+        const items = await youtube.playlistItems.list({
+          part: ['snippet', 'contentDetails'],
+          maxResults: 50,
+          playlistId: o.id,
+        });
+        return items.data;
+      }),
+    );
+
+    return result;
+  }
   async getMostVideo(userId: string) {
     const youtube = await this.authService.getYoutubeAPI(userId);
 
@@ -40,7 +72,7 @@ export class YoutubeService {
 
     // return category.data;
     const response = await youtube.videos.list({
-      part: ['snippet'],
+      part: ['snippet', 'contentDetails'],
       chart: 'mostPopular',
       videoCategoryId: '10',
       maxResults: 50,
@@ -51,9 +83,59 @@ export class YoutubeService {
   async getLikeVideo(userId: string) {
     const youtube = await this.authService.getYoutubeAPI(userId);
     const response = await youtube.videos.list({
-      part: ['snippet'],
+      part: ['snippet', 'contentDetails'],
       myRating: 'like',
       maxResults: 50,
+    });
+    return response.data;
+  }
+  async getSubscriber(userId: string) {
+    const youtube = await this.authService.getYoutubeAPI(userId);
+    const response = await youtube.subscriptions.list({
+      part: ['snippet'],
+      mine: true,
+      maxResults: 50,
+    });
+    return response.data;
+  }
+  async getRecentSearch(userId: string) {
+    const youtube = await this.authService.getYoutubeAPI(userId);
+    const response = await youtube.search.list({
+      part: ['snippet'],
+      order: 'date',
+      videoCategoryId: '10',
+      maxResults: 50,
+      regionCode: 'KR',
+      type: ['video'],
+    });
+    return response.data;
+  }
+  async getSearchData(userId: string, searchKeyword: string) {
+    const youtube = await this.authService.getYoutubeAPI(userId);
+    const response = await youtube.search.list({
+      part: ['snippet'],
+      maxResults: 50,
+      videoCategoryId: '10',
+      regionCode: 'KR',
+      type: ['video'],
+      order: 'viewCount',
+      q: searchKeyword,
+    });
+    return response.data;
+  }
+  async getDislikeVideos(userId: string) {
+    const youtube = await this.authService.getYoutubeAPI(userId);
+    const response = await youtube.videos.list({
+      part: ['snippet', 'contentDetails'],
+      myRating: 'dislike',
+      maxResults: 50,
+    });
+    return response.data;
+  }
+  async insertPlaylist(userId: string) {
+    const youtube = await this.authService.getYoutubeAPI(userId);
+    const response = await youtube.playlists.insert({
+      part: ['snippet'],
     });
     return response.data;
   }
